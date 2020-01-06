9e1543e57187070fd6215dae06955a1927e4d98a
Nick Tong <nicktong.001@gmail.com>
2020-01-06 18:40:30 -0500

    Tester assembly files

---
+++ Testing/test.asm
@@ -0,0 +1,20 @@
+    .target "65C02"
+    .org $8000
+
+reset:
+    lda #$ff
+    sta $6002
+
+    lda #$50
+    sta $6000
+
+loop:
+    ror
+    sta $6000
+
+    jmp loop
+
+    .org $fffc
+    .word reset
+    .word $0000
+
